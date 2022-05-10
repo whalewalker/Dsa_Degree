00f27e18137abc18b99792940136f8e8a98c95d0
whalewalker <mongodbms@gmail.com>
2022-05-10 20:05:04 +0100

    refactor consecutive solution and intuitive about the approach

--- src/com/algo/ConsecutiveOnes.java
+++ src/com/algo/ConsecutiveOnes.java
@@ -1,28 +1,30 @@
 package com.algo;
 
 public class ConsecutiveOnes {
+    /**
+     * Intuition
+     * Let first and last variables respectively represent the first and last index where a 1 appears.
+     * Let counter represent the number of 1's in the array.
+     *The only way for all ones to appear consecutively between [first,last] is for last-first + 1 == counter.
+     *  Thus, the problem can be solved by simply calculating these 3 values with one iteration.
+     * @param nums
+     * @return boolean
+     */
 
     public boolean solution(int[] nums) {
-        boolean lastOne = false;
+        int first = -1;
+        int last = -1;
         int counter = 0;
-        boolean end = false;
 
-        for (int num : nums) {
-            if (counter == 0 && num == 1){
-                lastOne = true;
-                counter++;
-            }
-
-            if (counter > 0 && num != 1){
-                end = true;
-            }
-
-            if (end && num == 1){
-                return false;
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] == 1){
+               if (first == -1) first = index;
+               last = index;
+               counter++;
             }
 
         }
-        return lastOne;
+        return last - first + 1 == counter;
 
     }
 }
